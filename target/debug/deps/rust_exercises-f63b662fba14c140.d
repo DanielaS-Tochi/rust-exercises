1904475c5c66fb57ff9afb8ef98dd5c96f504974
Daniela Silvana Tochi <danielastochi@gmail.com>
2025-07-10 20:59:11 -0300

    Initial commit

---
+++ target/debug/deps/rust_exercises-f63b662fba14c140.d
@@ -0,0 +1,5 @@
+/home/usuariod/solana-practice/rust-exercises/target/debug/deps/rust_exercises-f63b662fba14c140.d: src/main.rs
+
+/home/usuariod/solana-practice/rust-exercises/target/debug/deps/rust_exercises-f63b662fba14c140: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-exercises.d
@@ -0,0 +1,1 @@
+/home/usuariod/solana-practice/rust-exercises/target/debug/rust-exercises: /home/usuariod/solana-practice/rust-exercises/src/main.rs
